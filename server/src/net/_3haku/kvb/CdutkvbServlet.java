c20214a88d56187b4beb9cc0dbc387602b213ff7
baizhongwei@gmail.com <baizhongwei@gmail.com@e85bef5b-5ffe-ea1b-d3ba-d5401e8e8b05>
2010-08-06 10:30:09 +0000

    服务器版本修复  仍有bug

--- server/src/net/_3haku/kvb/CdutkvbServlet.java
+++ server/src/net/_3haku/kvb/CdutkvbServlet.java
@@ -1,9 +1,14 @@
+/**
+ * Copyright (c) 2010 princehaku
+ * All right reserved.
+ * Author princehaku
+ * Site http://3haku.net
+ * Created on : 2010-8-06, 10:26:40
+ */
+
 package net._3haku.kvb;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import javax.servlet.http.*;
 
@@ -12,6 +17,8 @@ import net._3haku.kvb.CourseTable.CourseTable;
 import net._3haku.kvb.TimeFactory.SchoolTime;
 import net._3haku.kvb.TimeFactory.WinterSchoolTime;
 import net._3haku.kvb.util.Parser;
+import net._3haku.kvb.util.Source;
+import net._3haku.kvb.util.StringUtil;
 
 @SuppressWarnings("serial")
 public class CdutkvbServlet extends HttpServlet {
@@ -20,44 +27,66 @@ public class CdutkvbServlet extends HttpServlet {
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.setCharacterEncoding("UTF-8");
+		//课表的HTML
 		String kbhtml = "";
-		FileInputStream fs = new FileInputStream("res/kb.html");
-		InputStreamReader read = new InputStreamReader(fs, "gb2312");
-		BufferedReader in = new BufferedReader(read);
-		while (in.ready())
-			kbhtml += in.readLine();
-		fs.close();
+		Source kvb=new Source();
+		if(req.getParameter("s")==null||req.getParameter("p")==null)
+		{
+			resp.getWriter().print("error");
+			resp.getWriter().close();
+			}
+		//学号
+		String sid=req.getParameter("s");
+		//密码
+		String pwd=req.getParameter("p");
+		int classnums=0;
+		//从教务处得到课表的HTML
+		//登陆
+		kvb.post("http://202.115.139.16/login.php","uname="+sid+"&upwd="+pwd+"&usertype=%D1%A7%C9%FA","gb2312");
+		//得到课表url
+		String res=kvb.get("http://202.115.139.16/sel_listsys/sel_listsys.php","gb2312");
+		if(res.equals(""))
+		{
+			resp.getWriter().print("error");
+			resp.getWriter().close();
+			}
+		String url=StringUtil.findMc(res, "\\\'查看所选课表总课表\\\' onclick=\\\"window.open\\(\\\'\\.\\.(.*?)\\\'\\,",1).replaceAll(" ","%20");
+		kbhtml=kvb.get("http://202.115.139.16"+url,"gb2312");
 		// 解析课表
 		try {
 			CourseTable Tb;
 			Tb = Parser.parseTable(kbhtml);
-			// System.out.println(Tb.getRowNums());
+			//计算总课数
 			SchoolTime st = new WinterSchoolTime();
-			resp.getWriter().print(Tb.getRowNums() + "@" + Tb.getCoursesNums());
+			for (int i = 1; i <= Tb.getRowNums(); i++) {
+				for (int o = 1; o <= Tb.getColumnNums(i); o++) {
+					CourseColumn cc = Tb.getColum(i, o);
+					if (cc.haveClass()) {classnums++;}
+				}
+				
+			}
+			resp.getWriter().print(Tb.getRowNums() + "@" + Tb.getCoursesNums()+"@"+classnums);
 			for (int i = 1; i <= Tb.getRowNums(); i++) {
 				// 一周开始的时间
 				int courest = 1;
 				for (int o = 1; o <= Tb.getColumnNums(i); o++) {
 					CourseColumn cc = Tb.getColum(i, o);
 					if (cc.haveClass()) {
-						resp.getWriter().print("|");
-						resp.getWriter().print(
-								cc.getCourseIdxName() + "@"
+						resp.getWriter().print("|"+cc.getCourseIdxName().replaceAll("\n", "") + "@"
 										+ cc.getCoursePlace());
-						resp.getWriter().print(
-								"@"
+						resp.getWriter().print("@"
 										+ st.getStartTimeAt(courest).getHours()
 										+ ":"
 										+ st.getStartTimeAt(courest)
 												.getMinutes());
 						courest = courest + cc.getCrossSpan();
-						resp.getWriter().print(
-								"@"
+						resp.getWriter().print("@"
 										+ st.getEndTimeAt(courest - 1)
 												.getHours()
 										+ ":"
 										+ st.getStartTimeAt(courest - 1)
 												.getMinutes());
+						
 					} else {
 						courest = courest + cc.getCrossSpan();
 					}
@@ -65,8 +94,11 @@ public class CdutkvbServlet extends HttpServlet {
 						courest = 1;
 				}
 			}
+			resp.getWriter().print("|end");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			resp.getWriter().print("error");
+			resp.getWriter().close();
 			e.printStackTrace();
 		}
 	}
