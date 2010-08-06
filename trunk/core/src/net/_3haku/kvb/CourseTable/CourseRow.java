8f95a3dbc483d9931c7c6d3c38ba0999a73943bf
baizhongwei@gmail.com <baizhongwei@gmail.com@e85bef5b-5ffe-ea1b-d3ba-d5401e8e8b05>
2010-08-06 08:25:12 +0000

    重新上传丢失的文件..

---
+++ trunk/core/src/net/_3haku/kvb/CourseTable/CourseRow.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2010 princehaku
+ * All right reserved.
+ * Author princehaku
+ * Site http://3haku.net
+ * Created on : 2010-7-22, 7:23:43
+ */
+
+package net._3haku.kvb.CourseTable;
+
+import java.util.ArrayList;
+
+/**课程行
+ *
+ * @author princehaku
+ */
+public class CourseRow extends  TableRow{
+    /**表头
+     *
+     */
+    private CourseRowHead courseRowHead;
+   
+    public CourseRowHead getRowHead() {
+        return courseRowHead;
+    }
+
+    public void setRowHead(CourseRowHead rowHead) {
+        this.courseRowHead = rowHead;
+    }
+
+   public ArrayList<CourseColumn> getCourseColums() {
+        ArrayList<CourseColumn> ccols=new ArrayList<CourseColumn>();
+         for(int i=0;i<colums.size();i++)
+         {
+            CourseColumn d=(CourseColumn)colums.get(i);
+            ccols.add(d);
+            //System.out.print(d.getCourseIdxName());
+         }
+        return ccols;
+    }
+   
+    public void setCourseColums(ArrayList<CourseColumn> cols) {
+         ArrayList<TableColumn> ccols=new ArrayList<TableColumn>();
+         for(int i=0;i<cols.size();i++)
+         {
+             TableColumn d=cols.get(i);
+             ccols.add(d);
+             //System.out.print(((CourseColumn)d).getCourseIdxName());
+         }
+         setColums(ccols);
+    }
+}
---
+++ trunk/core/src/net/_3haku/kvb/TimeFactory/TimeTable.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2010 princehaku
+ * All right reserved.
+ * Author princehaku
+ * Site http://3haku.net
+ * Created on : 2010-7-27, 21:56:23
+ */
+
+package net._3haku.kvb.TimeFactory;
+
+/**时间表
+ *
+ * @author princehaku
+ */
+public class TimeTable {
+    //重复开始时间
+
+    //重复结束时间
+
+    //每次的开始时间
+
+    //每次的结束时间
+
+    //星期几
+}
