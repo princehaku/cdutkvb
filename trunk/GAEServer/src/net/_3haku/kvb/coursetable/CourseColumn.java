323fd70a4e191b5083982f66939bedd21973226b
baizhongwei@gmail.com <baizhongwei@gmail.com@e85bef5b-5ffe-ea1b-d3ba-d5401e8e8b05>
2010-08-17 11:29:14 +0000

    核心包加入到里面

---
+++ trunk/GAEServer/src/net/_3haku/kvb/coursetable/CourseColumn.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2010 princehaku
+ * All right reserved.
+ * Author princehaku
+ * Site http://3haku.net
+ * Created on : 2010-7-20, 13:14:43
+ */
+
+package net._3haku.kvb.coursetable;
+
+/**课程单元格
+ * @author princehaku
+ */
+public class CourseColumn extends TableColumn implements CourseCellInfo{
+    /**是否有课
+     *
+     */
+    private boolean haveClass;
+    /**课程索引名
+     *
+     */
+    private String courseIdxName="";
+   /**上课的地点
+    *
+    */
+    private String coursePlace="";
+    /**构造新课程节点
+     * @deprecated
+     * @param haveClass
+     * @param crossSpan
+     * @param courseId
+     * @param courseTeacher
+     */
+    public CourseColumn(boolean haveClass,int crossSpan,String courseId,String courseTeacher) {
+       this.haveClass = haveClass;
+       setCourseIdxName(courseId);
+       setCrossSpan(crossSpan);
+       setCoursePlace(courseTeacher);
+    }
+
+    public CourseColumn() {
+        haveClass=true;
+    }
+    /**是否有课
+     *
+     * @return boolean
+     */
+    public boolean haveClass() {
+        return haveClass;
+    }
+    /**设置课程状态
+     * @param havaClass
+     */
+    public void setExtClass(boolean havaClass) {
+        haveClass=havaClass;
+    }
+    public String getCoursePlace() {
+        if(!haveClass())
+            return "No Class here";
+        return coursePlace;
+    }
+    /**设置上课的地点
+     *
+     * @param coursePlace
+     */
+    public void setCoursePlace(String coursePlace) {
+        this.coursePlace = coursePlace;
+    }
+    
+    public String getCourseIdxName() {
+        if(!haveClass())
+            return "No Class here";
+        return courseIdxName;
+    }
+    /**设置课程的索引名
+     * 对应课表节点
+     * @param courseId
+     */
+    public void setCourseIdxName(String courseIdxName) {
+        this.courseIdxName = courseIdxName;
+    }
+    @Override
+    public String toString()
+    {
+        return "课程索引名 :"+getCourseIdxName()+"上课地点 :"+getCoursePlace();
+    }
+}
---
+++ trunk/ServletServer/src/java/net/_3haku/kvb/bean/Course.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2010 princehaku
+ * All right reserved.
+ * Author princehaku
+ * Site http://3haku.net
+ * Created on : 2010-7-24, 14:43:16
+ */
+package net._3haku.kvb.bean;
+
+import net._3haku.kvb.time.TimeTable;
+
+import java.util.ArrayList;
+
+/**课程
+ *
+ * @author princehaku
+ */
+public class Course {
+
+    /**课程名
+     *
+     */
+    private String courseName;
+    /**课程id号
+     *
+     */
+    private String courseId;
+    /**课程教师
+     *
+     */
+    private String courseTeacher;
+    /**课程上课地点
+     *
+     */
+    private String coursePlace;
+    /**
+     * @param courseName
+     * @param courseId
+     * @param courseTeacher
+     * @param coursePlace
+     * @param courseScoure
+     * @param courseIdxName
+     * @param courseType
+     */
+    public Course(String courseName, String courseId, String courseTeacher, String coursePlace, String courseScoure, String courseIdxName, String courseType) {
+        this.courseName = courseName;
+        this.courseId = courseId;
+        this.courseTeacher = courseTeacher;
+        this.coursePlace = coursePlace;
+        this.courseScoure = courseScoure;
+        this.courseIdxName = courseIdxName;
+        this.courseType = courseType;
+    }
+    /**课程学分
+     *
+     */
+    private String courseScoure;
+    /**课程索引名
+     *
+     */
+    private String courseIdxName;
+    /**课程类型
+     * 比如:实验 理论
+     */
+    private String courseType;
+    /**时间表
+     *
+     */
+    private ArrayList<TimeTable> timeTables;
+    /**得到课程上课地点
+     * 
+     * @return
+     */
+    public String getCoursePlace() {
+        return coursePlace;
+    }
+    /**得到课程执教老师
+     * 
+     * @return
+     */
+    public String getCourseTeacher() {
+        return courseTeacher;
+    }
+    /**得到课程索引名
+     * 
+     * @return
+     */
+    public String getCourseIdxName() {
+        return courseIdxName;
+    }
+    /**得到课程课号
+     * 
+     * @return
+     */
+    public String getCourseId() {
+        return courseId;
+    }
+    /**得到课程源HTML
+     * 
+     * @return
+     */
+    public String getCourseScoure() {
+        return courseScoure;
+    }
+    /**得到课程全名
+     * 
+     * @return
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+    /**得到课程类型
+     * 
+     * @return
+     */
+    public String getCourseType() {
+        return courseType;
+    }
+
+    /**得到时间表
+     * @deprecated
+     * @return
+     */
+    public ArrayList<TimeTable> getTimeTables() {
+        return timeTables;
+    }
+
+    /**设置时间表
+     * @deprecated
+     * @return
+     */
+    public void setTimeTable(ArrayList<TimeTable> timeTables) {
+        this.timeTables = timeTables;
+    }
+
+    /**得到第 pos 个timetable
+     * @throws
+     * @param pos
+     * @return
+     */
+    public TimeTable getTimeTable(int pos) throws Exception {
+        if (timeTables.size() < pos) {
+            throw new Exception("Out of range");
+        }
+        TimeTable tt = timeTables.get(pos - 1);
+        return tt;
+    }
+}
