20a488466d51e611d9c935c0534a3a2238842c44
baizhongwei@gmail.com <baizhongwei@gmail.com@e85bef5b-5ffe-ea1b-d3ba-d5401e8e8b05>
2010-08-25 11:51:48 +0000

    这个servlet 用于分发jar包

---
+++ trunk/BuildKVB/src/java/net/_3haku/buildkvb/Combiner.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2010 princehaku
+ * All right reserved.
+ * Author princehaku
+ * Site http://3haku.net
+ * Created on : 24-八月-10, 下午 05:02
+ */
+
+package net._3haku.buildkvb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author princehaku
+ */
+public class Combiner implements Runnable{
+     String key="";
+     public Combiner(String key){
+        this.key=key;
+     }
+    public void run() {
+        Process p;
+        String cmd = "javac -g:none "+Builder.absTmpPath+"Key.java";
+        try {
+            p = Runtime.getRuntime().exec(cmd);
+            InputStream fis = p.getInputStream();
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            //System.out.println(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ trunk/BuildKVB/tmp/Key.java
@@ -0,0 +1,2 @@
+package net._3haku.key;
+public class Key { public static String key="de90d34c161dc4deadea53340087f8b4";}
